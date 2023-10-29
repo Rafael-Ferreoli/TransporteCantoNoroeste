c6448064c3d6082af3c258a3461907ffba436a08
Rafael Ferreira <rafaelferreoli@gmail.com>
2023-10-28 22:59:16 -0300

    mudança para canto noroeste, algoritmo pronto e criação de uma interface gráfica para facilitar o uso

---
+++ src/main/java/com/mycompany/transportecantonoroeste/TransporteCantoNoroeste.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+package com.mycompany.transportecantonoroeste;
+
+import view.FrHome;
+
+/**
+ *
+ * @author rafae
+ */
+public class TransporteCantoNoroeste {
+
+    public static void main(String[] args) {
+        FrHome fr = new FrHome();
+        fr.setVisible(true);
+
+    }
+}
